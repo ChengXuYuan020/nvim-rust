ba485154a3b2bbedc38911788ca9e28898eb31f2
ChengXuYuan020 <chnzm@126.com>
2022-05-07 19:20:37 +0800

    清理注释

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 require("basic.settings")
 require("basic.options")
 require("basic.mapping")
--- require("basic.config")
 require("basic.plugins")
 require("basic.after")
 
--- lua/basic/config.lua
+++
@@ -1,4 +0,0 @@
-vim.g.background_transparency = true
-
--- 指定代码片段存储路径
-vim.g.vsnip_snippet_dir = "~/.config/nvim/snippet"
